a0853aaf4a0799b57e7bf1a7d5c9485ec721ddec
dlavoieb <d.lavoie.boutin@me.com>
2013-10-31 09:40:12 -0400

    Add ps3 controll test

--- UI/Workspace/src/main.py
+++ UI/Workspace/src/main.py
@@ -4,5 +4,21 @@ import PyQt4.uic
 import PyQt4.QtCore
 import PyQt4.QtDesigner
 
-#Hello Mr. Lou
-print "Hello World"
+from PyQt4 import QtGui
+
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+
+    w = QtGui.QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
