ec57c325f6191a10df4f2a9bab31265ce63705cf
farazoman <farazoman@gmail.com>
2013-11-14 18:32:17 -0500

    fixed compiling errors and updated methods/fncs

--- ai/config/Config.cpp
+++ ai/config/Config.cpp
@@ -6,12 +6,11 @@ using namespace std;
 const char* taskFN = "tasks.xml"; //TODO change file name as neeeded
 const char* mapFN = "map.xml";
 
-pugi::xml_document loadFile(char* filename){
-	pugi::xml_document roboSub_doc;
+void loadFile(const char filename, pugi::xml_document& roboSub_doc){
 	pugi::xml_parse_result roboSub_result;
 	//TODO check if the file exists
 	
-	roboSub_result = roboSub_doc.load_file(filename);
+	roboSub_result = roboSub_doc.load_file(&filename);
 	if (roboSub_result){
    		std::cout << "XML [" << filename << "] loaded without errors\n\n";
 	}
@@ -22,33 +21,33 @@ pugi::xml_document loadFile(char* filename){
 	}
 
 	//deal with errors outside this to ensure program keeps running and errors are handeled accordingly
-	return load_Success;
 	
 }
 
 
-void parse(pugi::xml_document taskDoc, pugi::xml_document mapDoc){
+/*void parse(pugi::xml_document& taskDoc, pugi::xml_document& mapDoc){
 	pugi::xml_node tasks, task_child, map, map_child;
-	string task_list = "", map_list = "";
+	string task_list = "";
+	string map_list = "";
 
 
-	tasks = taskDoc.child("TASK_LIST");
-	map = mapDoc.child("MAP");	
+	tasks = taskDoc->child("TASK_LIST");
+	map = mapDoc->child("MAP");	
 	
 	task_child = tasks.child("task");
 	map_child = map.child("obj");
 	//TODO In this area add any parsing needed for the TASK_LIST for example number of attributtes to a tag and maybe number of tasks?
 	
 	for(task_child; task_child; task_child = task_child.next_sibling("task")){
-		task_list.append(task_child.next_attribute("name").value());
+		task_list.append(task_child.attribute("name").value());
 		//TODO add the other attributes here
 		task_list.append("\n");
 	}		
-	
+	cout  << task_list;
 	for(map_child; map_child; map_child = map_child.next_sibling("obj")){
 		std::cout << "2" << std::endl;
 	}
-}
+}*/
 
 
 Config::Config()
@@ -57,13 +56,12 @@ Config::Config()
 	pugi::xml_document roboSub_task_doc;
 	pugi::xml_document roboSub_map_doc;
 	
-	roboSub_task_doc = 
-	roboSub_map_doc = 
+	loadFile(taskFN, roboSub_task_doc);
+	loadFile(mapFN, roboSub_map_doc);
 
+	//parse(roboSub_task_doc&, roboSub_map_doc&);
 
 	//TODO create 
-	//Constructor:
-	int x = 0;
 	//Open file with name = MapStartOConfigFileName * .txt
 	//(so that starts with MapStartOConfigFileName, may have 
 	//lots of other descriptive stuff after that and then has .txt
@@ -85,7 +83,7 @@ void Config::GetTasks()
 }
 
 int main(){  //TODO Remove/COmment out later
-	loadFile();
-	parse();
+	Config();
+
 	return 0;
 }
