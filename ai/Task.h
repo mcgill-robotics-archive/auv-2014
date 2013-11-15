825ec5503a5bc838a82f60733df319550176a261
Sebastien <sebastien@ubuntu.(none)>
2013-11-15 01:15:18 -0500

    added invoker and sample tasks

--- ai/Task.h
+++ ai/Task.h
@@ -2,27 +2,33 @@
 #define Task_h
 
 #include "Logger.h"
-#include "Map.h"
-#include "ROS.h"
-#include <string>
+//#include "Map.h"
+//#include "ROS.h"
+//#include <string>
 
 
 class Task {
 
  public:
-	task(std::string task_name, ROS ros, Logger log, Map m ){
-		TaskName = task_name;
-		Ros = ros;
-		logger = log;
-		map = m;
-		// logger log -> created the 
+	// Task(std::string task_name, ROS ros, Logger log){ //Map m ){
+	// 	TaskName = task_name;
+	// 	Ros = ros;
+	// 	logger = log;
+	// 	//map = m;
+	// 	// logger log -> created the 
+	// }
+	Task(){
+		TaskName = "Default Task Name";
 	}
-    virtual void Execute();
+    virtual int Execute() = 0;
+    // {
+    // 	cout << "Default Execute" << endl;
+    // };
 
  private:
-    ROS Ros;
-    Logger logger;
-    Map map;
+    //ROS Ros;
+    //Logger logger;
+    //Map map;
 	std::string TaskName;
 };
 
---
+++ ai/TorpedoTask.cpp
@@ -0,0 +1,11 @@
+#include "Task.h"
+
+
+class TorpedoTask: public Task{
+public:
+	int Execute(){
+
+		std:cout << "Shoot Torpedo!!!"<< std::endl;
+		return 0;
+	}
+};
