fb119340612c1a8aae4f3794fa87f760d8785557
dlavoieb <d.lavoie.boutin@me.com>
2013-11-18 17:51:24 -0500

    file reorg

--- UI/QT design/Tests/PS3 Controller Interface/BuildSomething_Competition/tester.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-pygame.init()
-pygame.joystick.init()
-numberOfDeviceConnected = pygame.joystick.get_count()
-print numberOfDeviceConnected
-controller = pygame.joystick.Joystick(0)
-print controller.get_name()
