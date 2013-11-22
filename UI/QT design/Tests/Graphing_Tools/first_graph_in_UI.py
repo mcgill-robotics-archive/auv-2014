99507a4552e60e85084d2cfc70bc07efae8f6038
dlavoieb <d.lavoie.boutin@me.com>
2013-11-22 11:21:52 -0500

    first implementation if an updating in ros with pyqtgraph

--- UI/QT design/Tests/Graphing_Tools/first_graph_in_UI.py
+++ UI/QT design/Tests/Graphing_Tools/first_graph_in_UI.py
@@ -12,36 +12,42 @@ class Main(QtGui.QMainWindow):
         self.ui.setupUi(self)
         self.timer = QtCore.QTimer()
 
-#   MULTIPLE STATIC CURVES IN THE SAME PLOT
-#        #FILL THE PLOT
-        p2 = self.ui.graphicsView.addPlot(title="Multiple curves")
-        p2.plot(numpy.random.normal(size=100), pen=(255,0,0))
-        p2.plot(numpy.random.normal(size=100)+5, pen=(0,255,0))
-        p2.plot(numpy.random.normal(size=100)+10, pen=(0,0,255))
 
-        self.ui.graphicsView.nextRow()
-#   SINGLE UPDATING CURVE
-        self.timer.timeout.connect(self.update)
-        self.timer.start(50)
+        global lower_updating, data, p6, second_updating, data2, plot2, p2
 
-        global lower_updating, data, p6, second_updating, data2
-        p6 = self.ui.graphicsView.addPlot(title="Updating plot")
-        lower_updating = p6.plot()
-        second_updating = p6.plot(pen="y")
         data = []
         data2 =[]
         for i in range(0, 100, 01):
             data.append(i)
             data2.append(i+400)
 
+#   MULTIPLE STATIC CURVES IN THE SAME PLOT
+#        #FILL THE PLOT
+        p2 = self.ui.graphicsView.addPlot(title="Multiple curves")
+        plot2 = p2.plot(data2, pen=(255,0,0))
+
+
+        self.ui.graphicsView.nextRow()
+#   SINGLE UPDATING CURVE
+        p6 = self.ui.graphicsView.addPlot(title="Updating plot")
+        lower_updating = p6.plot()
+        second_updating = p6.plot(pen="y")
+
+        self.timer.timeout.connect(self.update)
+        self.timer.start(50)
+
+
+
     def update(self):
-        global lower_updating, data, data2, p6, second_updating
+        global lower_updating, data, data2, p6, second_updating, plot2, p2
      #   data.remove(data[0])
         data.append(data[len(data)-1]%3.14*31)
         data2.append(data2[len(data2)-1]%3.14*31)
         lower_updating.setData(data)
         second_updating.setData(data2)
+        plot2.setData(data2)
         p6.setXRange(len(data)-100, len(data))
+        p2.setXRange(len(data2)-100, len(data2))
        # p6.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
 
 
--- catkin_ws/src/front_end/scripts/mainApp.py
+++ catkin_ws/src/front_end/scripts/mainApp.py
@@ -19,7 +19,9 @@ class Main(QtGui.QMainWindow):
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.timer = QtCore.QTimer()
+        self.controller_timer = QtCore.QTimer()
+        self.plot_timer = QtCore.QTimer()
+        self.plot_timer.start(50)
 
         #create tuple list of checkboxes
         self.ps3 = PS3Controller.PS3Controller()
@@ -31,21 +33,34 @@ class Main(QtGui.QMainWindow):
         p2.plot(numpy.random.normal(size=100)+5, pen=(0,255,0))
         p2.plot(numpy.random.normal(size=100)+10, pen=(0,0,255))
 
+        #new row
+        self.ui.graphicsView.nextRow()
+
+#UPDATING PLOT
+        global med_curve_data, med_curve, updating_plot
+        med_curve_data = [1]
+        updating_plot = self.ui.graphicsView.addPlot(title = "Updating plot")
+        med_curve = updating_plot.plot(pen = "r")
+
+
+
+
         #buttons
         QtCore.QObject.connect(self.ui.actionQuit, QtCore.SIGNAL("triggered()"), self.close)
         QtCore.QObject.connect(self.ui.changeStatus, QtCore.SIGNAL("clicked()"), self.setTimer)
 
-        #timer
-        QtCore.QObject.connect(self.timer, QtCore.SIGNAL("timeout()"), self.controllerUpdate)
+        #timer connect
+        QtCore.QObject.connect(self.controller_timer, QtCore.SIGNAL("timeout()"), self.controllerUpdate)
+        QtCore.QObject.connect(self.plot_timer, QtCore.SIGNAL("timeout()"), self.plot_update)
 
     def setTimer(self):
         """
         Start the timer
         """
         if self.ui.ManualRB.isChecked():
-            self.timer.start(updateFrequency)
+            self.controller_timer.start(updateFrequency)
         elif self.ui.AutonomusRB.isChecked():
-            self.timer.stop()
+            self.controller_timer.stop()
 
     def controllerUpdate(self):
         """
@@ -69,6 +84,12 @@ class Main(QtGui.QMainWindow):
                 self.checkboxList[i].setChecked(False)
         publisherText = ps3_publisher.ps3_publisher(self.ps3.horizontal_front_speed, self.ps3.horizontal_side_speed, self.ps3.vertical_speed, self.ps3.pitch_speed, self.ps3.yaw_speed, 0)
         self.ui.textField.append(publisherText)
+
+    def plot_update(self):
+        global med_curve, med_curve_data, updating_plot
+        med_curve_data.append((med_curve_data[len(med_curve_data)-1]+1)*23%31)
+        med_curve.setData(med_curve_data)
+        updating_plot.setXRange(len(med_curve_data)-100, len(med_curve_data))
 
 
 if __name__ == "__main__":
