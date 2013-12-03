ac568b435b315b6f6c231a24ca6486ece3eb27c8
nickspeal <nickspeal@gmail.com>
2013-12-03 17:34:37 -0500

    created roadmap for depth controller

--- catkin_ws/src/controls/src/depthController.cpp
+++ catkin_ws/src/controls/src/depthController.cpp
@@ -1,2 +1,36 @@
+
+/*
+This is the first attempt at integrating a control system into the robosub simulation.
+It subscribes to desired and estimated velocity topics and publishes a new velocity topic according to the dynamics of the vehicle.
+
+Written by Nick Speal Dec 3.
+*/
+
+/*
+Roadmap
+
+Header stuff
+Subscribers
+	actual velocity - from simulator
+	cmd_vel - (incomplete) from front-end
+	d_des - from front-end
+	pose of robot - from simulator
+Position Controller
+	computes thrust based on depth error
+Dynamics simulator
+	computes velocity based on all forces, including thrust
+publishers
+	cmd_vel - merge 5-DOF from frontend with z_vel from here
+
+
+*/
 #include "ros/ros.h"
 #include 
+
+// Subscribers
+
+//Position Controller
+
+//Dynamics simulator
+
+//Publishers
