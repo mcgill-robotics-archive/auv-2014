cee4d6ea957eb9e963c5654d0854f3f150d166fc
nickspeal <nickspeal@gmail.com>
2013-12-03 19:20:11 -0500

    placed scripts back into new controls package

---
+++ catkin_ws/src/controls/src/depthController.cpp
@@ -0,0 +1,56 @@
+
+/*
+This is the first attempt at integrating a control system into the robosub simulation.
+It subscribes to desired and estimated velocity topics and publishes a new velocity topic according to the dynamics of the vehicle.
+
+Handwritten notes in page 79 of Nick's design notebook for P&C.
+
+Written by Nick Speal Dec 3.
+*/
+
+/*
+Roadmap
+
+Header stuff
+Subscribers
+	actual velocity - from simulator
+	cmd_vel - (incomplete) from front-end
+	d_des - from front-end
+	pose of robot - from simulator
+Position Controller
+	computes thrust based on depth error
+Dynamics simulator
+	computes velocity based on all forces, including thrust
+publishers
+	cmd_vel - merge 5-DOF from frontend with z_vel from here
+
+
+*/
+#include "ros/ros.h"
+#include "geometry_msgs"
+#include depthController.h
+
+void cmdVelCallback(const geometry_msgs::Twist)
+{
+	ROS_INFO("Subscriber received twist")
+}
+void main(int argc, char **argv)
+{
+	ros::init(argc,argv,"depthController");
+	ros::NodeHandle n;
+	ros::Subscriber sub = n.subscribe("cmd_vel", 1000, cmdVelCallback);
+	ros::spin();
+	return 0;
+
+}
+
+
+
+
+// Subscribers
+
+//Position Controller
+
+//Dynamics simulator
+
+//Publishers
