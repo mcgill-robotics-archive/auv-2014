b6b822f9c3d40c7966469c06564b84381e2d6c31
CompSys <mcgillroboticcompsys@gmail.com>
2013-12-04 03:02:35 -0500

    debugging depthController. added lots of semicolons. still doesnt work - i need to ask someone with roscpp experience

--- catkin_ws/src/controls/src/depthController.cpp
+++ catkin_ws/src/controls/src/depthController.cpp
@@ -33,38 +33,39 @@ publishers
 // using namespace std; //what does this do?!?!
 
 //global vars
-float zdes = 0;
-float zest = 0;
+float z_des = 0;
+float z_est = 0;
 
-input_xdot = 0;
-input_ydot = 0;
-input_zdot = 0;
-input_alphadot = 0;
-input_betadot = 0;
-input_gammadot = 0;
+float input_xdot = 0;
+float input_ydot = 0;
+float input_zdot = 0;
+float input_alphadot = 0;
+float input_betadot = 0;
+float input_gammadot = 0;
 
 void partial_cmd_vel_callback(const geometry_msgs::Twist twistMsg)
 {
 	//from frontend, missing z velocity for now
 	ROS_INFO("Subscriber received twist");
-	input_xdot = twistMsg.data.linear.x;
-	input_ydot = twistMsg.data.linear.y;
-	input_zdot = twistMsg.data.linear.z;
-	input_alphadot = twistMsg.data.angular.x;
-	input_betadot = twistMsg.data.angular.y;
-	input_gammadot = twistMsg.data.angular.z;
+	input_xdot = twistMsg.linear.x;
+	input_ydot = twistMsg.linear.y;
+	input_zdot = twistMsg.linear.z;
+	input_alphadot = twistMsg.angular.x;
+	input_betadot = twistMsg.angular.y;
+	input_gammadot = twistMsg.angular.z;
 }
 
 void zdes_callback(const std_msgs::Float64 data)
 {
 	ROS_INFO("Subscriber received zdes");
-	zdes = data.data;
+	z_des = data.data;
 }
 
-void pose_callback(const geometry_msgs::pose data)
+void pose_callback(const geometry_msgs::Pose& data)
+
 {
 	ROS_INFO("Subscriber received zest");
-	zest = data.position.z
+	z_est = data.position.z;
 }
 
 
@@ -74,12 +75,12 @@ int main(int argc, char **argv)
 	float m = 30; //mass in kg
 	float g = 9.81;
 	float cd = 0.1; //arbitrary drag coefficient
-	float buoyancy = 0.02; %percent buoyancy
-	float dt = 0.01 //temporary! TODO update this dynamically
+	float buoyancy = 0.02; // %percent buoyancy
+	float dt = 0.01; //temporary! TODO update this dynamically
 
 	//initializations
-	float zdot_old = 0; initial velocity
-	float zdot_new = zdot_old;
+	float zdot_old = 0; //initial velocity
+	float zdot_new = 0;
 	float Tz = 0;
 
 	// ROS subscriber setup
@@ -92,10 +93,10 @@ int main(int argc, char **argv)
 
 	ros::Rate loop_rate(100); //100 hz??
 	
-	ready = 0
+	bool ready = 0;
 	while (ready == 0)
 	{
-		ready = 1		 
+		ready = 1;		 
 		if (partial_cmd_vel_subscriber.getNumPublishers() == 0) ready = 0;
 		if (zdes_subscriber.getNumPublishers() == 0) ready = 0;
 		if (pose_subscriber.getNumPublishers() == 0) ready = 0;
@@ -106,7 +107,7 @@ int main(int argc, char **argv)
 		ros::spinOnce();
 		
 		// Position Controller
-		Tz = 0.1*(z_des-z_est)
+		Tz = 0.1*(z_des-z_est);
 
 		//Dynamics simulator
 		zdot_new = dt*(Tz/m - buoyancy*g - cd*zdot_old/m) + zdot_old;
@@ -115,7 +116,8 @@ int main(int argc, char **argv)
 		// publish here
 		std::stringstream ss;
 		ss << "Speed: " << zdot_new;
-		ROS_INFO("%s", ss.str())
+		//ROS_INFO("%s", ss.str());
+                ROS_INFO("%s", zdot_new);
 
 
 		
