efe1a7c9a3e595453d6f315aa89842b0079223a5
nickspeal <nickspeal@gmail.com>
2013-12-04 02:20:40 -0500

    first attempt at throrough subscriber for depthController

--- catkin_ws/src/controls/src/depthController.cpp
+++ catkin_ws/src/controls/src/depthController.cpp
@@ -13,9 +13,8 @@ Roadmap
 
 Header stuff
 Subscribers
-	actual velocity - from simulator
-	cmd_vel - (incomplete) from front-end
-	d_des - from front-end
+	partial_cmd_vel - from front-end
+	zdes - from front-end
 	pose of robot - from simulator
 Position Controller
 	computes thrust based on depth error
@@ -28,29 +27,103 @@ publishers
 */
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include "std_msgs/Float64.h"
 #include "depthController.h"
 
-void cmdVelCallback(const geometry_msgs::Twist twistMsg)
+// using namespace std; //what does this do?!?!
+
+//global vars
+float zdes = 0;
+float zest = 0;
+
+input_xdot = 0;
+input_ydot = 0;
+input_zdot = 0;
+input_alphadot = 0;
+input_betadot = 0;
+input_gammadot = 0;
+
+void partial_cmd_vel_callback(const geometry_msgs::Twist twistMsg)
 {
+	//from frontend, missing z velocity for now
 	ROS_INFO("Subscriber received twist");
+	input_xdot = twistMsg.data.linear.x;
+	input_ydot = twistMsg.data.linear.y;
+	input_zdot = twistMsg.data.linear.z;
+	input_alphadot = twistMsg.data.angular.x;
+	input_betadot = twistMsg.data.angular.y;
+	input_gammadot = twistMsg.data.angular.z;
 }
+
+void zdes_callback(const std_msgs::Float64 data)
+{
+	ROS_INFO("Subscriber received zdes");
+	zdes = data.data;
+}
+
+void pose_callback(const geometry_msgs::pose data)
+{
+	ROS_INFO("Subscriber received zest");
+	zest = data.position.z
+}
+
+
 int main(int argc, char **argv)
 {
+	//Parameters
+	float m = 30; //mass in kg
+	float g = 9.81;
+	float cd = 0.1; //arbitrary drag coefficient
+	float buoyancy = 0.02; %percent buoyancy
+	float dt = 0.01 //temporary! TODO update this dynamically
+
+	//initializations
+	float zdot_old = 0; initial velocity
+	float zdot_new = zdot_old;
+	float Tz = 0;
+
+	// ROS subscriber setup
 	ros::init(argc,argv,"depthController");
 	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("/gazebo/robot_twist", 1000, cmdVelCallback);
-	ros::spin();
+	ros::Subscriber partial_cmd_vel_subscriber = n.subscribe("partial_cmd_vel", 1000, partial_cmd_vel_callback);
+	ros::Subscriber zdes_subscriber = n.subscribe("zdes", 1000, zdes_callback);
+	ros::Subscriber pose_subscriber = n.subscribe("pose", 1000, pose_callback);
+
+
+	ros::Rate loop_rate(100); //100 hz??
+	
+	ready = 0
+	while (ready == 0)
+	{
+		ready = 1		 
+		if (partial_cmd_vel_subscriber.getNumPublishers() == 0) ready = 0;
+		if (zdes_subscriber.getNumPublishers() == 0) ready = 0;
+		if (pose_subscriber.getNumPublishers() == 0) ready = 0;
+	}
+
+	while(ros::ok())
+	{
+		ros::spinOnce();
+		
+		// Position Controller
+		Tz = 0.1*(z_des-z_est)
+
+		//Dynamics simulator
+		zdot_new = dt*(Tz/m - buoyancy*g - cd*zdot_old/m) + zdot_old;
+
+		
+		// publish here
+		std::stringstream ss;
+		ss << "Speed: " << zdot_new;
+		ROS_INFO("%s", ss.str())
+
+
+		
+
+		loop_rate.sleep();
+	}
+
+
 	return 0;
 
 }
-
-
-
-
-// Subscribers
-
-//Position Controller
-
-//Dynamics simulator
-
-//Publishers
