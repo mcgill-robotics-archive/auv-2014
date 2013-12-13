bf6e32840f37c44ae167417d592defb239beb1ff
Baycken <baycken@baycken-MacBookPro.(none)>
2013-12-13 18:15:54 -0500

    add motor msg and motor control arduino code

--- dotfiles/Symlink.sh
+++ dotfiles/Symlink.sh
@@ -3,7 +3,7 @@
 # .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
-
+source /opt/ros/hydro/setup.bash 
 ########## Variables
 
 dir=$PWD   # dotfiles directory
