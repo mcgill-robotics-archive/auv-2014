c5464d045e71d04714d68a04aacd1cbce60ef852
Sebastien <sebastien@ubuntu.(none)>
2014-01-05 15:36:48 -0500

    changed folder structure of the planner team

---
+++ catkin_ws/src/planner/include/Invoker.h
@@ -0,0 +1,24 @@
+#ifndef Invoker_h
+#define Invoker_h
+
+#include <iostream>
+#include <vector>
+#include "Task.hpp"
+//#include "Logger.h"
+//#include "Map.h"
+//#include "ROS.h"
+
+
+
+class Invoker{
+
+ public:
+ 	Invoker();
+ 	void AddTask(Task * t);
+ 	void StartRun();
+ private:
+ 	std::vector<Task *> TaskList;
+
+};
+
+#endif // Task_h
