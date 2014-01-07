83614d5de77c5f1fb4fca405f525f5cf82da3a9f
Alex Reiff <areiff19@gmail.com>
2014-01-07 13:15:40 -0500

    added planner's interface for passing to motor control

--- catkin_ws/src/planner/src/Interface.h
+++ catkin_ws/src/planner/src/Interface.h
@@ -3,7 +3,8 @@
 #include "geometry_msgs/Pose.h"
 #include "std_msgs/Float64.h"
 #include "std_msgs/Int64.h"
-//#include "planner/CVQuery.h"
+#include "std_msgs/String.h"
+#include "planner/MotorControl.h"
 
 #ifndef Interface_h
 #define Interface_h
