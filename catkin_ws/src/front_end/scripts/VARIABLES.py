5949ac7fa27452facd038864cf3f165f9c7c607e
dlavoieb <d.lavoie.boutin@me.com>
2014-01-07 23:26:11 -0500

    added in code documentation

--- catkin_ws/src/front_end/scripts/VARIABLES.py
+++ catkin_ws/src/front_end/scripts/VARIABLES.py
@@ -1,9 +1,15 @@
-#__author__ = 'david'
-from  PyQt4 import QtCore
+"""
+file containing all variables shared between one or more files
+also contains parameters and constants used in the UI and the key mapping for the keyboard controller
+__author__ = 'david'
+"""
+from PyQt4 import QtCore
+
 
 class vel_vars(object):
-
-    #variables shared by all controller for the cmd_vel and z_position
+    """
+    variables shared by all controller for the cmd_vel and z_position
+    """
     pitch_velocity = 0
     yaw_velocity = 0
     x_velocity = 0
@@ -17,7 +23,9 @@ class vel_vars(object):
 
 
 class KeyMapping(object):
-    # Here we define the keyboard map for our controller
+    """
+    Here we define the keyboard map for our controller
+    """
     PitchForward = QtCore.Qt.Key_I
     PitchBackward = QtCore.Qt.Key_K
     YawLeft = QtCore.Qt.Key_J
@@ -30,17 +38,26 @@ class KeyMapping(object):
     DecreaseY = QtCore.Qt.Key_D
     Surface = QtCore.Qt.Key_H
 
+
 class ROS_Topics(object):
-    battery_voltage="battery_voltage"
-    pressure="pressure"
-    depth="depth"
+    """
+    variables for the ros topic names
+    """
+    battery_voltage = "battery_voltage"
+    pressure = "pressure"
+    depth = "depth"
     left_pre_topic = "/my_robot/camera1/image_raw"
     imu_pose = "pose"
     partial_cmd_vel = '/gazebo/robot_twist'
     zdes = 'zdes'
 
+
 class misc_vars(object):
+    """
+    various parameters and constants
+    """
     controller_updateFrequency = 50
     low_battery_threshold = 2.0
     max_voltage = 24.0
     GUI_UPDATE_PERIOD = 20 #ms
+    length_plot = 25
