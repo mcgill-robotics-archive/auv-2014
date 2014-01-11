041418bbc266a175ed10dc97acf6c8aa7799493b
Alex Reiff <areiff19@gmail.com>
2014-01-11 12:26:52 -0500

    moved headers and added planner to control method

--- catkin_ws/src/planner/include/Interface.h
+++ catkin_ws/src/planner/include/Interface.h
@@ -2,43 +2,30 @@
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Pose.h"
 #include "std_msgs/Float64.h"
+#include "std_msgs/String.h"
 #include "std_msgs/Int64.h"
-//#include "planner/CVQuery.h"
+#include "simulator/ThrusterForces.h"
 
 #ifndef Interface_h
 #define Interface_h
 
 #include "Interface.h"
 
-
 class Interface {
-
- public:
-
+  public:
     Interface();
     
-    void setDepth();
-
     double getDepth();
-
-    void setPosition();
-
-    double GetX();
-
-    double GetY();
-
-//    PoseObj GetPose();
-
-//    Vector Velocity();
-
-    void setPressure();
+    void setDepth(const std_msgs::Float64::ConstPtr& msg);
 
     double getPressure();
+    void setPressure(const std_msgs::Float64::ConstPtr& msg);
+
+    void TODO(const geometry_msgs::Twist msg);
+
+    void setOrientation(const geometry_msgs::Quaternion msg);
 
     int main(int argc,char **argv);
-
- private:
-    double Depth;
 };
 
 #endif // Interface_h
