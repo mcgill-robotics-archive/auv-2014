b0421c7e5930d119007d3a5641a69750e12f7072
Jean-Sebastien Dery <jeansebastien.dery@gmail.com>
2014-01-11 12:36:50 -0500

    Made changes in the CV ROS Message and added a test node

---
+++ catkin_ws/src/computer_vision/include/CVTestNode.h
@@ -0,0 +1,26 @@
+#ifndef CV_TEST_NODE_H
+#define CV_TEST_NODE_H
+
+#include <ros/ros.h>
+#include <ros/callback_queue.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <list>
+
+class CVTestNode {
+
+	private:
+	
+	
+	public:
+	
+	CVTestNode(ros::NodeHandle& nodeHandle);
+	~CVTestNode();
+	
+	private:
+	
+
+};
+	
+#endif
