e7c902c78e95e826c36aecd6cc832ef52d204c1f
McGill Robotics <robotics@McGill.(none)>
2014-01-14 14:40:15 -0500

    lol

--- catkin_ws/src/hydrophones/scripts/hydrophones.py
+++ catkin_ws/src/hydrophones/scripts/hydrophones.py
@@ -12,46 +12,33 @@ import sys
 
 # VARIABLES
 NUMBER_OF_MICS = 4  # SELF-EXPLANATORY
-DISTANCE = 4      # DISTANCE BETWEEN HYDROPHONES IN CENTIMETERS
+DISTANCE = 4        # DISTANCE BETWEEN HYDROPHONES IN CENTIMETERS
 SPEED = 0.001450    # SPEED OF SOUND UNDER WATER IN METERS PER MICROSECOND
 DIMENSIONS = 3      # DIMENSIONS OF SOLUTION
 
 mics = [[] for x in xrange(NUMBER_OF_MICS)]
-pos0 = [[] for x in xrange(DIMENSIONS)]
-pos1 = [[] for x in xrange(DIMENSIONS)]
-
-pnt0 = rospy.Publisher('pos0', PointStamped)
-pnt1 = rospy.Publisher('pos1', PointStamped)
-rospy.init_node('hydrophones')
+pos = [[] for x in xrange(DIMENSIONS)]
 count = 0
+
+pnt = rospy.Publisher('pos', PointStamped)
+rospy.init_node('hydrophones')
 
 # ROS TOPIC
 def publish():
     global count
 
-    pointStamp0 = PointStamped()
-    pointStamp0.header.seq = count
-    pointStamp0.header.stamp = rospy.rostime.Time.now()
-    pointStamp0.header.frame_id = 'map'
-    pointStamp0.point.x = pos0[0][0]
-    pointStamp0.point.y = pos0[1][0]
-    pointStamp0.point.z = pos0[2][0]
-
-    pointStamp1 = PointStamped()
-    pointStamp1.header.seq = count
-    pointStamp1.header.stamp = rospy.rostime.Time.now()
-    pointStamp1.header.frame_id = 'map'
-    pointStamp1.point.x = pos1[0][0]
-    pointStamp1.point.y = pos1[1][0]
-    pointStamp1.point.z = pos1[2][0]
+    pointStamp = PointStamped()
+    pointStamp.header.seq = count
+    pointStamp.header.stamp = rospy.rostime.Time.now()
+    pointStamp.header.frame_id = 'map'
+    pointStamp.point.x = pos[0][0]
+    pointStamp.point.y = pos[1][0]
+    pointStamp.point.z = pos[2][0]
 
     count += 1
 
-    rospy.loginfo(pointStamp0)
-    pnt0.publish(pointStamp0)
-
-    rospy.loginfo(pointStamp1)
-    pnt1.publish(pointStamp1)
+    rospy.loginfo(pointStamp)
+    pnt.publish(pointStamp)
 
 # ESTABLISH CONNECTION
 def connect():
@@ -83,6 +70,10 @@ def parse(str):
     elif str[0] is 'Z':
         mics[3].append(int(str[1:]) + 980)
 
+# MAGNITUDE OF VECTOR
+def magnitude(x, y, z):
+    return sqrt(x * x + y * y )       
+
 # TRIANGULATE
 def triangulate(index):
     # TIME DIFFERENCES
@@ -105,45 +96,56 @@ def triangulate(index):
     y1 = (pow(DISTANCE, 4) - DISTANCE * DISTANCE * sx * sx - DISTANCE * DISTANCE * sz * sz + DISTANCE * DISTANCE * sx * sy + DISTANCE * DISTANCE * sz * sy - sy * pow(sz, 3) + sy * sy * sx * sx - sy * pow(sx, 3) + sy * sy * sz * sz - DISTANCE * DISTANCE * sy * sy + sqrt(-pow(sy, 4) * pow(sx, 4) - pow(sy, 4) * pow(sz, 4) + 4 * pow(sy, 4) * sz * sz * DISTANCE * DISTANCE + pow(sx, 4) * sy * sy * DISTANCE * DISTANCE - 4 * sy * sy * sx * sx * pow(DISTANCE, 4) - 4 * sy * sy * sz * sz * pow(DISTANCE, 4) - 2 * pow(sy, 3) * pow(sx, 3) * DISTANCE * DISTANCE + 2 * pow(sy, 3) * sx * pow(DISTANCE, 4) - 2 * pow(sy, 3) * pow(sz, 3) * DISTANCE * DISTANCE + 2 * pow(sy, 3) * sz * pow(DISTANCE, 4) + pow(sz, 4) * sy * sy * DISTANCE * DISTANCE + 4 * pow(sy, 4) * sx * sx * DISTANCE * DISTANCE + 3 * sy * sy * pow(DISTANCE, 6) - 4 * pow(sy, 4) * pow(DISTANCE, 4) - pow(sx, 4) * sy * sy * sz * sz - pow(sz, 4) * sy * sy * sx * sx + 4 * sz * sz * sy * sy * sx * sx * DISTANCE * DISTANCE - 2 * DISTANCE * DISTANCE * pow(sy, 5) * sx - 2 * DISTANCE * DISTANCE * pow(sy, 5) * sz + DISTANCE * DISTANCE * pow(sy, 6) + 2 * pow(sy, 5) * pow(sx, 3) + 2 * pow(sy, 5) * pow(sz, 3) - pow(sy, 6) * sx * sx - pow(sy, 6) * sz * sz + 2 * sy * sy * pow(sz, 3) * pow(sx, 3) - 2 * DISTANCE * DISTANCE * sz * sy * sy * pow(sx, 3) + 2 * pow(DISTANCE, 4) * sx * sy * sy * sz - 2 * DISTANCE * DISTANCE * sx * sy * sy * pow(sz, 3))) / (DISTANCE * DISTANCE - sy * sy - sx * sx - sz * sz) / DISTANCE / 2;
     z1 = (DISTANCE * DISTANCE * sy + sz * (pow(DISTANCE, 4) - DISTANCE * DISTANCE * sx * sx - DISTANCE * DISTANCE * sz * sz + DISTANCE * DISTANCE * sx * sy + DISTANCE * DISTANCE * sz * sy - sy * pow(sz, 3) + sy * sy * sx * sx - sy * pow(sx, 3) + sy * sy * sz * sz - DISTANCE * DISTANCE * sy * sy + sqrt(-pow(sy, 4) * pow(sx, 4) - pow(sy, 4) * pow(sz, 4) + 4 * pow(sy, 4) * sz * sz * DISTANCE * DISTANCE + pow(sx, 4) * sy * sy * DISTANCE * DISTANCE - 4 * sy * sy * sx * sx * pow(DISTANCE, 4) - 4 * sy * sy * sz * sz * pow(DISTANCE, 4) - 2 * pow(sy, 3) * pow(sx, 3) * DISTANCE * DISTANCE + 2 * pow(sy, 3) * sx * pow(DISTANCE, 4) - 2 * pow(sy, 3) * pow(sz, 3) * DISTANCE * DISTANCE + 2 * pow(sy, 3) * sz * pow(DISTANCE, 4) + pow(sz, 4) * sy * sy * DISTANCE * DISTANCE + 4 * pow(sy, 4) * sx * sx * DISTANCE * DISTANCE + 3 * sy * sy * pow(DISTANCE, 6) - 4 * pow(sy, 4) * pow(DISTANCE, 4) - pow(sx, 4) * sy * sy * sz * sz - pow(sz, 4) * sy * sy * sx * sx + 4 * sz * sz * sy * sy * sx * sx * DISTANCE * DISTANCE - 2 * DISTANCE * DISTANCE * pow(sy, 5) * sx - 2 * DISTANCE * DISTANCE * pow(sy, 5) * sz + DISTANCE * DISTANCE * pow(sy, 6) + 2 * pow(sy, 5) * pow(sx, 3) + 2 * pow(sy, 5) * pow(sz, 3) - pow(sy, 6) * sx * sx - pow(sy, 6) * sz * sz + 2 * sy * sy * pow(sz, 3) * pow(sx, 3) - 2 * DISTANCE * DISTANCE * sz * sy * sy * pow(sx, 3) + 2 * pow(DISTANCE, 4) * sx * sy * sy * sz - 2 * DISTANCE * DISTANCE * sx * sy * sy * pow(sz, 3))) / (DISTANCE * DISTANCE - sy * sy - sx * sx - sz * sz) - DISTANCE * DISTANCE * sz + sy * sy * sz - sy * sz * sz) / DISTANCE / sy / 2;
 
-    pos0[0].append(x0)
-    pos0[1].append(y0)
-    pos0[2].append(z0)
+    # SELECT CORRECT SOLUTION
+    minTime = min(dtx, min(dty, dtz))
+    if dtx == minTime:
+        if magnitude(x0 - DISTANCE, y0, z0) < magnitude(x1 - DISTANCE, y1, z1):
+            x = x0
+            y = y0
+            z = z0
+        else:
+            x = x1
+            y = y1
+            z = z1
+    elif dty == minTime:
+        if magnitude(x0, y0 - DISTANCE, z0) < magnitude(x1, y1 - DISTANCE, z1):
+            x = x0
+            y = y0
+            z = z0
+        else:
+            x = x1
+            y = y1
+            z = z1
+    else:
+        if magnitude(x0, y0, z0 - DISTANCE) < magnitude(x1, y1, z1 - DISTANCE):
+            x = x0
+            y = y0
+            z = z0
+        else:
+            x = x1
+            y = y1
+            z = z1
 
-    pos1[0].append(x1)
-    pos1[1].append(y1)
-    pos1[2].append(z1)
+    pos[0].append(x)
+    pos[1].append(y)
+    pos[2].append(z)
 
 # AVERAGE VALUES
 def average():
     xSum = 0
     ySum = 0
     zSum = 0
-    length = len(pos0[0])
+    length = len(pos[0])
 
     if length > 0:
         for i in xrange(length):
-            xSum += pos0[0][i]
-            ySum += pos0[1][i]
-            zSum += pos0[2][i]
+            xSum += pos[0][i]
+            ySum += pos[1][i]
+            zSum += pos[2][i]
 
-        pos0[0][0] = xSum / length
-        pos0[1][0] = ySum / length
-        pos0[2][0] = zSum / length
-
-    xSum = 0
-    ySum = 0
-    zSum = 0
-    length = len(pos1[0])
-
-    if length > 0:
-        for i in xrange(length):
-            xSum += pos1[0][i]
-            ySum += pos1[1][i]
-            zSum += pos1[2][i]
-
-        pos1[0][0] = xSum / length
-        pos1[1][0] = ySum / length
-        pos1[2][0] = zSum / length
+        pos[0][0] = xSum / length
+        pos[1][0] = ySum / length
+        pos[2][0] = zSum / length
 
 # MAIN
 try:
@@ -162,12 +164,12 @@ try:
                         # CHECK IF NO FUCKUPS
                         if len(mics[0]) > 0 and len(mics[0]) == len(mics[1]) and len(mics[1]) == len(mics[2]) and len(mics[2]) == len(mics[3]):
                             for i in xrange(len(mics[0])):
-                                try:
-                                    triangulate(i)
-                                    average()
-                                    publish()
-                                except:
-                                    print 'degenerate triangle'
+                                #try:
+                                triangulate(i)
+                                average()
+                                publish()
+                                #except:
+                                 #   print 'degenerate triangle'
                         elif len(mics[0]) > 0 and len(mics[1]) > 0 and len(mics[2]) > 0 and len(mics[3]) > 0:
                             try:
                                 triangulate(0)
@@ -182,7 +184,7 @@ try:
                         for i in xrange(NUMBER_OF_MICS):
                             del mics[i][:]
                         for i in xrange(DIMENSIONS):
-                            del pos0[i][:]
+                            del pos[i][:]
                     else:
                         parse(line)
                 except rospy.ROSInterruptException:
