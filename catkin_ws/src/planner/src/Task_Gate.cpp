66b52ad77082c4fdf3b27b083c6cd52944900efe
SebastienLC <seb.lemieux.codere@gmail.com>
2014-01-14 21:00:37 -0500

    minor changes

--- catkin_ws/src/planner/src/Task_Gate.cpp
+++ catkin_ws/src/planner/src/Task_Gate.cpp
@@ -6,12 +6,13 @@ Task_Gate::Task_Gate() {
 
 int Task_Gate::Execute() {
 
-	ros::Rate loop_rate(5);
+	ros::Rate loop_rate(50);
+	loop_rate.sleep();
 
 	std::cout << ">> Executing Gate Task" << std::endl;
 	
 	std::cout<<"Gate Task setting velocity:   setVelocity(1, 1, 1, 1) "<<std::endl;
-	setVelocity(1, 1, 1, 1);	
+	setVelocity(1.0, 1.0, 1.0, 1.0);	
 	loop_rate.sleep();
 
 	std::cout<<"Gate Task setting position:   setPosition(1.0, 2.0, 3.0, 4.0, 5.0);"<<std::endl;
