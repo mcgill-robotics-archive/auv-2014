8f685d7894f0e6cb350418bb9867b34610583847
Baycken <bei.liu@mail.mcgill.ca>
2014-01-18 14:50:02 -0500

    update arduino code to reflex msg package relocation

--- Arduino/MotorControl/MotorControl.ino
+++ Arduino/MotorControl/MotorControl.ino
@@ -1,12 +1,13 @@
 #include <Servo.h> 
 #include <ros.h>
 #include <std_msgs/Int16.h>
-#include <motor_msgs/motor.h>
+#include <arduino_msgs/motor.h>
+#include <arduino_msgs/solenoid.h>
 Servo myservo[6];
 ros::NodeHandle nh;
 
 
-void messageCb( const motor_msgs::motor& msg){
+void motorCb( const arduino_msgs::motor& msg){
   myservo[0].writeMicroseconds(1500 + msg.x_p);
   myservo[1].writeMicroseconds(1500 + msg.x_n);  // blink the led
   myservo[2].writeMicroseconds(1500 + msg.y_p);
@@ -15,7 +16,7 @@ void messageCb( const motor_msgs::motor& msg){
   myservo[5].writeMicroseconds(1500 + msg.z_n);
 }
 
-ros::Subscriber<motor_msgs::motor> sub("/motor", &messageCb );
+ros::Subscriber<arduino_msgs::motor> sub("/motor", &motorCb );
 
 void setup(){
   myservo[0].attach(2); 
@@ -23,7 +24,7 @@ void setup(){
   myservo[2].attach(4);  
   myservo[3].attach(5);  
   myservo[4].attach(6);  
-  myservo[5].attach(7);  
+  myservo[5].attach(7);
   nh.initNode();
   nh.subscribe(sub);
 }
