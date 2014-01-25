7b5e414ead91a653201dadec82223a91af7d7f96
Alex Reiff <areiff19@gmail.com>
2014-01-25 14:04:14 -0500

    added dummy CV node for planner to test with

---
+++ catkin_ws/src/planner/src/CVTest.h
@@ -0,0 +1,16 @@
+#ifndef CVTest_h
+#define CVTest_h
+
+#include "ros/ros.h"
+#include "gazebo_msgs/ModelStates.h"
+#include "computer_vision/VisibleObjectData.h"
+
+class CVTest {
+
+ public:
+     int main(int argc, char **argv);
+
+};
+
+#endif // rosTest_h
+
