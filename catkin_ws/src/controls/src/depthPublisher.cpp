0822ce633b6767e7c9250eae1c1616b746acb336
MK <mikeking214@gmail.com>
2014-01-26 17:02:43 -0500

    created depthPublisher node to publish just the depth

---
+++ catkin_ws/src/controls/src/depthPublisher.cpp
@@ -0,0 +1,49 @@
+#include "ros/ros.h"
+#include "gazebo_msgs/ModelStates.h"
+#include "std_msgs/Float64.h"
+
+
+
+// global vars
+
+double z_position;
+
+
+void estimatedDepth_callback(gazebo_msgs::ModelStates data) // subscribe to model states
+  {
+    z_position = data.pose[0].position.z;
+}
+
+int main(int argc, char **argv)
+{
+
+
+// create node called "depthPublisher"
+  ros::init(argc, argv, "depthPublisher");
+  ros::NodeHandle n;
+
+  // parameters
+
+  std_msgs::float64 depthVal;
+  double surfaceHeight;
+  n.param<double>("surfaceHeight", surfaceHeight, 10);
+
+  ros::Publisher depthPub = n.advertise<std_msgs::float64>("depthCalculated", 1000); // publish to topic called "depthCalculated"
+
+  ros::Rate loop_rate(10);
+
+  while (ros::ok())
+  {
+
+    depthVal.data = surfaceHeight - z_position;
+
+    depthPub.publish(depthVal);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+  }
+
+
+  return 0;
+}
