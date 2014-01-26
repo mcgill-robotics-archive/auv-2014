68719727b9163382d0eaa53b9eaafc958b4aa9d5
MK <mikeking214@gmail.com>
2014-01-26 14:34:35 -0500

    Created publishers to publish fake planner messages for testing controls

--- catkin_ws/src/planner/src/CurrentCVTaskPublisher.cpp
+++ catkin_ws/src/planner/src/CurrentCVTaskPublisher.cpp
@@ -4,8 +4,13 @@
 #include <sstream>
 
 /**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
+
+New node to publish hard coded strings to mimick commands coming from the planner
+Used to unit test other code (controls, etc..)
+
+*/
+
+
 int main(int argc, char **argv)
 {
   /**
@@ -18,7 +23,7 @@ int main(int argc, char **argv)
    * You must call one of the versions of ros::init() before using any other
    * part of the ROS system.
    */
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "FakeTaskPublisher");
 
   /**
    * NodeHandle is the main access point to communications with the ROS system.
@@ -27,57 +32,28 @@ int main(int argc, char **argv)
    */
   ros::NodeHandle n;
 
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  ros::Publisher taskPubFront = n.advertise<std_msgs::String>("currentCVTask_Front", 1000);
+  ros::Publisher taskPubDown = n.advertise<std_msgs::String>("currentCVTask_Down", 1000);
+
 
   ros::Rate loop_rate(10);
 
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-  int count = 0;
   while (ros::ok())
   {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
-    std_msgs::String msg;
 
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
+    std_msgs::String msgFront;
+    std_msgs::String msgDown;
 
-    ROS_INFO("%s", msg.data.c_str());
+    msgFront.data = "gate";
+    msgDown.data = "";
 
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
-    chatter_pub.publish(msg);
+    taskPubFront.publish(msgFront);
+    taskPubDown.publish(msgDown);
+
 
     ros::spinOnce();
 
     loop_rate.sleep();
-    ++count;
   }
 
 
