deaaeb9659dae4f23b66e41872e360d1eb452110
McGill Robotics <robotics@McGill.(none)>
2014-01-28 16:41:10 -0500

    controls gains tuning

--- catkin_ws/src/controls/src/testControls.cpp
+++ catkin_ws/src/controls/src/testControls.cpp
@@ -92,7 +92,7 @@ int main(int argc, char **argv)
 		msg.Pitch.data = setPoint_Pitch;
 
 		//isactive parameters doesnt work yet so these are the parameters 1/28
-		msg.XPos.isActive = 0;
+		msg.XPos.isActive = 1;
 		msg.YPos.isActive = 1;
 		msg.Depth.isActive = 1;
 		msg.Yaw.isActive = 0;
