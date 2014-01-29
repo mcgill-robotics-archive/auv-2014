a307467613cbd3da0e3a93274fbfc80565825ed8
Baycken <bei.liu@mail.mcgill.ca>
2014-01-28 22:18:54 -0400

    merge arduino code

--- Arduino/MotorControl/MotorControl.ino
+++ Arduino/MotorControl/MotorControl.ino
@@ -3,27 +3,12 @@
 #include <std_msgs/Int16.h>
 #include <arduino_msgs/motor.h>
 #include <arduino_msgs/solenoid.h>
-#include <arduino_msgs/batteryLevel.h>
+
+ros::NodeHandle nh;
+std_msgs::Int16 depth_msg;
 
 Servo myservo[6];
 
-
-#define MAX_ANALOG 1023  // in bits
-#define MAX_ARDUINO_VOLTAGE 5000 // in mV
-#define RESISTANCE 100.0 // In ohms. Set to the value of the resistor being used. Should be less than 250 ohms because 5V / 20mA (max current) = 250 omhs.
-
-#define BASE_CURRENT 4.0
-#define CURRENT_RANGE 16.0   // from 4.0 mA to 20.0 mA
-#define MAX_DEPTH 914.4      // In centimeters. 30 feet
-#define OFFSET 7.0          // Just substract it. May need to be recalculated when circuit is built.
-std_msgs::Int16 depth_msg;
-ros::Publisher depth("depth", &depth_msg);  // Publish the depth topic
-
-int sensorPin = A0;      // select the input pin for the potentiometer
-float sensorValue = 0;  // variable to store the value coming from the sensor
-long depthSensorSchedule = 0;
-long batteryLevelSchedule = 0;
-ros::NodeHandle nh;
 
 int boundCheck(int x){
   if(x> 500 || x< -500){
@@ -55,41 +40,33 @@ ros::Subscriber<arduino_msgs::solenoid> solenoid_sub("/elec_interface/solenoic",
 ros::Subscriber<arduino_msgs::motor> motor_sub("/elec_interface/motor", &motorCb );
 
 void setup(){
+ 
+  //MotorControl setup
   myservo[0].attach(2); 
   myservo[1].attach(3);  
   myservo[2].attach(4);  
   myservo[3].attach(5);  
   myservo[4].attach(6);  
   myservo[5].attach(7);
+  
+  //SolenoidValve setup
   pinMode(0,OUTPUT);
   pinMode(1,OUTPUT);
   pinMode(2,OUTPUT);
   pinMode(3,OUTPUT);
   pinMode(4,OUTPUT);
   pinMode(5,OUTPUT);
+  
+  //ros node initializtion
   nh.initNode();
-  nh.advertise(depth);
-  nh.subscribe(motor_sub);
-  nh.subscribe(solenoid_sub);
+  nh.subscribe(motor_sub);    //motor 
+  nh.subscribe(solenoid_sub); // solenoid
 }
+
+
+
+
 void loop(){
-  long currentTime = millis();
-  
-  if(depthSensorSchedule < currentTime){
-  sensorValue = analogRead(sensorPin);                                             // ADC bits
-  sensorValue = sensorValue*MAX_ARDUINO_VOLTAGE/(float)MAX_ANALOG;                 // From ADC to voltage in mV
-  sensorValue = sensorValue/RESISTANCE;                                            // Get current in mA
-  sensorValue = (int)(((sensorValue-BASE_CURRENT)/CURRENT_RANGE)*MAX_DEPTH);       // C0nvert the current to depth
-  
-  depth_msg.data = sensorValue - OFFSET;
-  depth.publish(&depth_msg);
-  depthSensorSchedule += 100;  
-  }
-  
-  if(batteryLevelSchedule < currentTime){
-  
-  batteryLevelSchedule += 10000;
-  }  
   nh.spinOnce();
   delay(1);
 }
