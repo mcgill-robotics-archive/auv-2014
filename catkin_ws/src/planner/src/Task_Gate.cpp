678d1fc03e11259fd57372dfd05ba291b20df267
Mathieu Wang <mathieu1330@hotmail.com>
2014-02-08 17:33:03 -0500

    Update Task_Gate.cpp
    Add pseudocode for planner's gate strategy. Need to wait for control and simulator to implementing and testing

--- catkin_ws/src/planner/src/Task_Gate.cpp
+++ catkin_ws/src/planner/src/Task_Gate.cpp
@@ -7,12 +7,23 @@ Task_Gate::Task_Gate() {
 int Task_Gate::Execute() {
 	ros::Rate loop_rate(50);
 	loop_rate.sleep();
-
+	
 	std::cout << ">> Executing Gate Task" << std::endl;
 	weAreHere("Beginning GATE task");	loop_rate.sleep();
 
 	std::cout<<"Gate Task setting position relative to gate:  setPosition(-2, -1, 0, 0, 0, 8.8) "<<std::endl;
 	setVisionObj(1);	loop_rate.sleep();
+	
+	/*once simulator and controls work:
+	
+	add method:
+	while (currentPos != desiredPos) {
+		setPosition(desiredPos)
+	}
+	
+	Set pos to (2.0, 0, 0, 0, 8.8) first
+	
+	*/
 	setPosition(-2.0, -1, 0, 0.0, 8.8);	loop_rate.sleep();
 
 	std::cout<<"Gate Task completed"<<std::endl;
