f921a7509161ff70826b86a76234b1c3b444a274
Max Krogius <maxkrogius@gmail.com>
2014-02-21 14:38:18 +0000

    First draft of dead reckoning

--- catkin_ws/src/state_estimation/src/ros_node.py
+++ catkin_ws/src/state_estimation/src/ros_node.py
@@ -1,8 +1,9 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
-from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Vector3Stamped
 from std_msgs.msg import Float64
 import dead_reck
+import math
 
 #TODO: Finish everything in this file.
 
@@ -13,15 +14,20 @@ def cvCallback(poseStamped):
     estimator.updateCV([pos.x, pos.y, pos.z])
 
 def imuCallback(poseStamped):
-    orientation = poseStampe.pose.orientation
+    q = poseStamped.pose.orientation
 
-    estimator.updateOrientation([orientation.w, orientation.x, orientation.y, orientation.z])
+    sin = math.sqrt(q.x*q.x + q.y*q.y + q.z*q.z)
+    cos = q.w
+    angle = 2*math.atan2(sin,cos)
 
-def velCallback(velVector):
-    estimator.updateVelocity([velVector.x, velVector.y, velVector.z])
+    estimator.updateOrientation(angle)
+
+def velCallback(velStamped):
+    estimator.updateVelocity(velStamped.vector.x, velStamped.vector.y
+                            ,velStamped.header.stamp.secs + 1e-9*velStamped.header.stamp.nsecs)
 
 def depthCallback(depth):
-    estimator.updateVelocity(depth)
+    estimator.updateDepth(depth)
 
 # Init the ros node, subscribers and publishers
 # And run the node
@@ -33,7 +39,7 @@ def init():
     # Subscribe to different inputing topics
     rospy.Subscriber('cv_topic', PoseStamped, cvCallback)
     rospy.Subscriber('imu_topic', PoseStamped, imuCallback)
-    rospy.Subscriber('vel_topic', Vector3, velCallback)
+    rospy.Subscriber('vel_topic', Vector3Stamped, velCallback)
     rospy.Subscriber('depth_topic', Float64, depthCallback)
 
     # Publish the filtered data to a topic
