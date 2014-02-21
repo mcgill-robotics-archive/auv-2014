b3cb8440350d19eacdb18e2eaedb4b704d773b60
Max Krogius <maxkrogius@gmail.com>
2014-02-21 14:18:36 +0000

    First draft of dead reckoning

--- catkin_ws/src/state_estimation/src/dead_reck.py
+++ catkin_ws/src/state_estimation/src/dead_reck.py
@@ -1,31 +1,60 @@
-
+import math
 
 class dead_reck():
-    #Makes the assumption that pitch and roll are always zero
+    #This estimator makes the assumption that pitch and roll are always zero
 
     hasTarget = False
-    position = [0,0,0]
-    yawOfTarget = 0
+
+    x = 0
+    y = 0
+
     #I'm assuming here that the targets have IDs
     targetID = 0
-    velocity = [0,0,0]
-    bearing = 0
-    heading = 0
-    pitch = 0
-    roll = 0
+
+    #Velocity
+    time = -123456789
+
+    #These angles are along z = down. AKA measured clockwise
+    yawOfTarget = 0
+    heading = -123456789
+
 
     def __init__(self):
         pass
 
     def updateOrientation(self, newHeading):
+        if self.heading != -123456789:
+            headingChange = newHeading - self.heading
+            self.yawOfTarget -= headingChange
+            self.x = self.x*math.cos(headingChange) + self.y*math.sin(headingChange)
+            self.y = self.y*math.cos(headingChange) - self.x*math.sin(headingChange)
+        self.heading = newHeading
 
-        pass
+    def updateCV(self, hasTarget, targetID, x, y, yawOfTarget):
+        if hasTarget:
+            self.targetID = targetID
+            self.x = x
+            self.y = y
+            self.yawOfTarget = yawOfTarget
+        self.hasTarget = hasTarget
 
-    def updateCV(self):
-        pass
 
-    def updateVelocity(self):
-        pass
+
+    def updateVelocity(self, vx, vy, time):
+        if self.time != -123456789:
+            timeDelta = time - self.time
+            self.x += vx*timeDelta
+            self.y += vy*timeDelta
+        self.time = time
+
 
     def getState(self):
-        pass
+        return {
+                'hasTarget':hasTarget
+                ,'x':x
+                ,'y':y
+                ,'targetID':targetID
+                ,'time':time
+                ,'yawOfTarget':yawOfTarget
+                ,'heading':heading
+                }
