1a6eedbed0cad48ec123eadda07ed55aca0cbe60
Nikhil <acenicks@Reboot.(none)>
2014-03-16 14:07:16 -0400

    Updated the TF Static Broadcaster

--- catkin_ws/src/tf_utilities/src/static_broadcaster.cpp
+++ catkin_ws/src/tf_utilities/src/static_broadcaster.cpp
@@ -12,15 +12,146 @@ int main(int argc, char** argv) {
 		broadcaster.sendTransform(
 			// Transform data, quaternion for rotations and vector3 for translational vectors
 			tf::StampedTransform(
-				tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.0)),
+				tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(1.0, 0.5, 0.5)),
 				// Give it a time stamp
 				ros::Time::now(),
 				// from
 				"base_link",
 				// to
-				"base_laser"
+				"IMU"
 			)
 		);
+		
+		broadcaster.sendTransform(
+			// Transform data, quaternion for rotations and vector3 for translational vectors
+			tf::StampedTransform(
+				tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3( 2.0, 1.0, 1.0)),
+				// Give it a time stamp
+				ros::Time::now(),
+				// from
+				"base_link",
+				// to
+				"camera_right"
+			)
+		);
+		
+		broadcaster.sendTransform(
+			// Transform data, quaternion for rotations and vector3 for translational vectors
+			tf::StampedTransform(
+				tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(2.0, 0.0, 1.0)),
+				// Give it a time stamp
+				ros::Time::now(),
+				// from
+				"base_link",
+				// to
+				"camera_left"
+			)
+		);
+		
+		broadcaster.sendTransform(
+			// Transform data, quaternion for rotations and vector3 for translational vectors
+			tf::StampedTransform(
+				tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(2.0, 0.5, 1.0)),
+				// Give it a time stamp
+				ros::Time::now(),
+				// from
+				"base_link",
+				// to
+				"camera_down"
+			)
+		);
+		
+		broadcaster.sendTransform(
+			// Transform data, quaternion for rotations and vector3 for translational vectors
+			tf::StampedTransform(
+				tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(1.7, 0.5, 1.0)),
+				// Give it a time stamp
+				ros::Time::now(),
+				// from
+				"base_link",
+				// to
+				"grabber"
+			)
+		);
+		
+		broadcaster.sendTransform(
+			// Transform data, quaternion for rotations and vector3 for translational vectors
+			tf::StampedTransform(
+				tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.7, 0.0, 1.0)),
+				// Give it a time stamp
+				ros::Time::now(),
+				// from
+				"base_link",
+				// to
+				"marker_left"
+			)
+		);
+		
+		broadcaster.sendTransform(
+			// Transform data, quaternion for rotations and vector3 for translational vectors
+			tf::StampedTransform(
+				tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.7, 1.0, 1.0)),
+				// Give it a time stamp
+				ros::Time::now(),
+				// from
+				"base_link",
+				// to
+				"marker_right"
+			)
+		);
+			
+		broadcaster.sendTransform(
+			// Transform data, quaternion for rotations and vector3 for translational vectors
+			tf::StampedTransform(
+				tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(2.0, 0.0, 0.5)),
+				// Give it a time stamp
+				ros::Time::now(),
+				// from
+				"base_link",
+				// to
+				"torpedo_left"
+			)
+		);
+		
+		broadcaster.sendTransform(
+			// Transform data, quaternion for rotations and vector3 for translational vectors
+			tf::StampedTransform(
+				tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(2.0, 1.0, 0.5)),
+				// Give it a time stamp
+				ros::Time::now(),
+				// from
+				"base_link",
+				// to
+				"torpedo_right"
+			)
+		);
+		
+		broadcaster.sendTransform(
+			// Transform data, quaternion for rotations and vector3 for translational vectors
+			tf::StampedTransform(
+				tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(2.0, 0.5, 0.5)),
+				// Give it a time stamp
+				ros::Time::now(),
+				// from
+				"base_link",
+				// to
+				"contact_bouy"
+			)
+		);
+		
+		broadcaster.sendTransform(
+			// Transform data, quaternion for rotations and vector3 for translational vectors
+			tf::StampedTransform(
+				tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(2.0, 0.5, 0.5)),
+				// Give it a time stamp
+				ros::Time::now(),
+				// from
+				"base_link",
+				// to
+				"contact_bouy"
+			)
+		);		
+		
 		
 		r.sleep();
 	}
