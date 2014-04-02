187e79d8425e899450d9bdb8c9d53218f67a1c86
Michael Elliot King <michael@MKs-MacBookPro.(none)>
2014-04-02 17:10:40 -0400

    Changed gui_controls

--- catkin_ws/src/controls/src/gui_controls.cpp
+++ catkin_ws/src/controls/src/gui_controls.cpp
@@ -1,21 +1,36 @@
-#include "controls.h" //should I make a different header?
+#include "gui_controls.h" //should I make a different header?
 
 /* 
 GUI for controls gains
 Makes sliders for each gain appear with integer and decimal control
 Created by Matt Mayers Apr 2
 */
+
 int main(int argc, char **argv)
 {
 	//Create ROS Node
 	ros::init(argc,argv,"gui_controls");
 	ros::NodeHandle n;
 
-
+	float kp_xPos;
+	float ki_xPos;
+	float kd_xPos;
+	float kp_yPos;
+	float ki_yPos;
+	float kd_yPos;
+	float kp_Depth;
+	float ki_Depth;
+	float kd_Depth;
+	float kp_Yaw;
+	float ki_Yaw;
+	float kd_Yaw;
+	float kp_Pitch;
+	float ki_Pitch;
+	float kd_Pitch;
 
 
 		// Variables used for the trackbars window.
-		const bool isUsingControlTrackbarWindow_xPos = false;
+		const bool isUsingControlTrackbarWindow_xPos = true;
 		const std::string CONTROL_TRACKBARS_WINDOW_XPOS = "control_trackbars_window_xpos";
 
 		const bool isUsingControlTrackbarWindow_yPos = false;
@@ -27,7 +42,7 @@ int main(int argc, char **argv)
 		const bool isUsingControlTrackbarWindow_Yaw = true;
 		const std::string CONTROL_TRACKBARS_WINDOW_YAW = "control_trackbars_window_yaw";
 
-		const bool isUsingControlTrackbarWindow_Pitch = false;
+		const bool isUsingControlTrackbarWindow_Pitch = true;
 		const std::string CONTROL_TRACKBARS_WINDOW_PITCH = "control_trackbars_window_pitch";
 
 		int kp_xPos_dec = 0;
@@ -146,6 +161,63 @@ int main(int argc, char **argv)
 	    ROS_INFO("Controls::The window should be instantiated.");
 
 
+	while(ros::ok())
+	{
+		ros::spinOnce();	//Updates all variables
+
+	    kp_xPos = kp_xPos_int + kp_xPos_dec/10;
+	    ki_xPos = ki_xPos_int + ki_xPos_dec/10;
+	    kd_xPos = kd_xPos_int + kd_xPos_dec/10;
+
+
+	    kp_yPos = kp_yPos_int + kp_yPos_dec/10;
+	    ki_yPos = ki_yPos_int + ki_yPos_dec/10;
+	    kd_yPos = kd_yPos_int + kd_yPos_dec/10;
+
+
+	    kp_Depth = kp_Depth_int + kp_Depth_dec/10;
+	    ki_Depth = ki_Depth_int + ki_Depth_dec/10;
+	    kd_Depth = kd_Depth_int + kd_Depth_dec/10;
+
+	    kp_Yaw = kp_Yaw_int + kp_Yaw_dec/10;
+	    ki_Yaw = ki_Yaw_int + ki_Yaw_dec/10;
+	    kd_Yaw = kd_Yaw_int + kd_Yaw_dec/10;
+
+	    kp_Pitch = kp_Pitch_int + kp_Pitch_dec/10;
+	    ki_Pitch = ki_Pitch_int + ki_Pitch_dec/10;
+	    kd_Pitch = kd_Pitch_int + kd_Pitch_dec/10;
+
+	    ROS_INFO(("Controls::kp_xPos value " + boost::lexical_cast<std::string>(kp_xPos)).c_str());
+
+	    //Refreshes the window.
+	    if (isUsingControlTrackbarWindow_xPos) {
+	    	//Wait 1 milisecond to listen to events from trackbars
+	    	cv::waitKey(1);
+	    }	
+
+
+
+	    n.setParam("/gains/kp_xPos", kp_xPos);
+	    n.setParam("/gains/ki_xPos", ki_xPos);
+	    n.setParam("/gains/kd_xPos", kd_xPos);
+
+	    n.setParam("/gains/kp_yPos", kp_yPos);
+	    n.setParam("/gains/ki_yPos", ki_yPos);
+	    n.setParam("/gains/kd_yPos", kd_yPos);	    
+
+
+	    n.setParam("/gains/kp_Depth", kp_Depth);
+	    n.setParam("/gains/ki_Depth", ki_Depth);
+	    n.setParam("/gains/kd_Depth", kd_Depth);	    
+
+	    n.setParam("/gains/kp_Yaw", kp_Yaw);
+	    n.setParam("/gains/ki_Yaw", ki_Yaw);
+	    n.setParam("/gains/kd_Yaw", kd_Yaw);	    
+
+	    n.setParam("/gains/kp_Pitch", kp_Pitch);
+	    n.setParam("/gains/ki_Pitch", ki_Pitch);
+	    n.setParam("/gains/kd_Pitch", kd_Pitch);	    
+
 
 		if (isUsingControlTrackbarWindow_xPos) {
 			ROS_INFO("Freeing memory used by the trackbar window.");
@@ -177,7 +249,7 @@ int main(int argc, char **argv)
 			cv::destroyWindow(CONTROL_TRACKBARS_WINDOW_PITCH);
 		}
 
-		return 0;
-
-	
+		
+	}
+	return 0;
 }
