af821110cbe74883a2a91aafe76e59c18126810c
McGill Robotics <robotics@McGill.(none)>
2014-04-06 23:28:32 -0400

    nick added arduino code to hardcode values to the motors and i guess someone created a sensor.sh script.

---
+++ catkin_ws/launch/sensor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Launching sensor nodes";
+
+roslaunch ~/McGill_RoboSub_2014/catkin_ws/launch/arduino.launch;
+roslaunch computer_vision camera_down.launch;
+roslaunch computer_vision camera_front_left.launch;
+roslaunch computer_vision camera_front_right.launch;
