147b381be6adce986abc0d3848d6618ad6a4a291
ernacktob <gogo_resto12@yahoo.ca>
2014-04-06 22:57:01 -0400

    corrected depth python node

--- catkin_ws/src/state_estimation/src/depth_node.py
+++ catkin_ws/src/state_estimation/src/depth_node.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 from std_msgs.msg import Int16
 from std_msgs.msg import Float64
@@ -17,9 +19,9 @@ def depthCallBack(sensor_reading):
     current_depth = ((depth_reading * max_arduino_voltage / max_analog / resistance - base_current) / current_range) * max_depth - offset
     current_depth = current_depth / 100.0	# To meters
     
-	# Apply Filters
-	depths.append(current_depth)
-	depth = depth + (current_depth - depths.popleft()) / WINDOW_WIDTH
+    # Apply Filters
+    depths.append(current_depth)
+    depth = depth + (current_depth - depths.popleft()) / WINDOW_WIDTH
 
     # Publish
     pub1.publish(current_depth)
@@ -42,20 +44,20 @@ def init():
     pub1 = rospy.Publisher('/state_estimation/rawDepth', Float64)
     pub2 = rospy.Publisher('/state_estimation/filteredDepth', Float64)
     
-	# Constants
-	max_analog = 1023  			# in bits
-	max_arduino_voltage = 5000 	# in mV
-	resistance = 100.0 			# In ohms. Set to the value of the resistor being used. Should be less than 250 ohms because 5V / 20mA (max current) = 250 omhs.
-	base_current = 4.0
-	current_range = 16.0   		# from 4.0 mA to 20.0 mA
-	max_depth = 914.4      		# In centimeters. 30 feet
-	offset = 7.0          		# Just substract it. May need to be recalculated when circuit is built.
+    # Constants
+    max_analog = 1023  			# in bits
+    max_arduino_voltage = 5000 	# in mV
+    resistance = 100.0 			# In ohms. Set to the value of the resistor being used. Should be less than 250 ohms because 5V / 20mA (max current) = 250 omhs.
+    base_current = 4.0
+    current_range = 16.0   		# from 4.0 mA to 20.0 mA
+    max_depth = 914.4      		# In centimeters. 30 feet
+    offset = 7.0          		# Just substract it. May need to be recalculated when circuit is built.
 
     rospy.spin()
 
 
 if __name__ == '__main__':
     try:
-		init()
+       init()
     except rospy.ROSInterruptException:
 	pass
