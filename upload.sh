db19adcf84197a53d1b6f88195e1ddb52b69684f
HUmar104 <humar104@gmail.com>
2014-04-19 22:48:51 -0700

    Fixing small mistake

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
-#~/bin/bash
+#!/bin/bash
 
 sshpass -p 'elgordo21' scp -r catkin_ws/src robotics@10.0.0.1:~/McGill_RoboSub_2014/catkin_ws
 sshpass -p 'elgordo21' scp -r Arduino robotics@10.0.0.1:~/McGill_RoboSub_2014/
