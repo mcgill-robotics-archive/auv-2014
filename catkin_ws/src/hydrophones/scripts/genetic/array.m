60088fb6a86c8c2d63c039e415bdc05993ff4302
Anass Al-Wohoush <anass.alwohoush@outlook.com>
2014-05-06 15:44:15 -0400

    added hydrophone array matlab simulator

---
+++ catkin_ws/src/hydrophones/scripts/genetic/array.m
@@ -0,0 +1,39 @@
+classdef array
+    properties
+        size        % number of receivers
+        receivers   % list of receivers
+        speed       % speed of sound    m/s
+        solution    % sound source location
+        error       % error from solution
+    end
+    
+    methods
+        function obj = array(size,speed)
+        % constructor
+            if (nargin > 0)
+                obj.size = size;
+                obj.receivers = receiver.empty(size,0);
+                obj.receivers(1) = receiver(0,0);
+                obj.speed = speed;
+                obj.solution = point();
+                obj.error = 0;
+            end
+        end
+
+        function obj = time_difference(obj,pos)
+        % compute time difference of each receiver
+            obj.receivers(1) = obj.receivers(1).time_of_travel(pos.x,pos.y,obj.speed);
+            for i = 2:obj.size
+                obj.receivers(i) = obj.receivers(i).time_of_travel(pos.x,pos.y,obj.speed);
+                obj.receivers(i).time = obj.receivers(1).time ...
+                                      - obj.receivers(i).time;
+            end
+            obj.receivers(1).time = 0;
+        end
+
+        function obj = compute_error(obj,pos)
+        % compute time difference of each receiver
+            obj.error = norm([obj.solution.x-pos.x obj.solution.y-pos.y]) / norm([pos.x pos.y]);
+        end
+    end
+end
