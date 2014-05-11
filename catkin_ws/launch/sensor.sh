86c4c995ce24b507f5cff3dabf26765a6cd71949
mike-n-7 <mnoseworthy7@gmail.com>
2014-05-11 18:45:58 -0400

    Vidometer added. Make sure to uncomment line in Planner.cpp

--- catkin_ws/launch/sensor.sh
+++ catkin_ws/launch/sensor.sh
@@ -6,3 +6,4 @@ roslaunch ~/McGill_RoboSub_2014/catkin_ws/launch/arduino.launch;
 roslaunch computer_vision camera_down.launch;
 roslaunch computer_vision camera_front_left.launch;
 roslaunch computer_vision camera_front_right.launch;
+roslaunch viso2_ros demo.launch
