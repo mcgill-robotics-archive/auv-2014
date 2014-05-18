728fd12ed8411ba87a226414fd3604ef56d651d8
Anass Al-Wohoush <anass.alwohoush@outlook.com>
2014-05-17 20:42:26 -0400

    fixed upload not removing old files

--- scripts/uploadGitRepoToRobot.sh
+++ scripts/uploadGitRepoToRobot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sshpass -p 'elgordo21' ssh robotics@10.0.0.1 -t "cd ~/McGill_RoboSub_2014/ && rm -rf catkin_ws/src Arduino setup"
 sshpass -p 'elgordo21' scp -r ../catkin_ws/src robotics@10.0.0.1:~/McGill_RoboSub_2014/catkin_ws
 sshpass -p 'elgordo21' scp -r ../Arduino robotics@10.0.0.1:~/McGill_RoboSub_2014/
 sshpass -p 'elgordo21' scp -r ../setup robotics@10.0.0.1:~/McGill_RoboSub_2014/
