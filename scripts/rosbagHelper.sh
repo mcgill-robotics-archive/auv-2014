79e727071fac17ba6af9c40ee9f1ace63cf88948
Jean-Sebastien Dery <jean-sebastien.dery@mail.mcgill.ca>
2014-05-18 12:45:28 -0400

    Added functions in the rosbagHelper

--- scripts/rosbagHelper.sh
+++ scripts/rosbagHelper.sh
@@ -9,12 +9,15 @@ command_arguments=''
 # Displays the usage of this script.
 usage() {
     echo USAGE: rosbagHelper.sh OPTION
-    echo "options: -a : all topics"  
-    echo "         -i : imu pose data"
-    echo "         -t : all temperature"
+    echo "options: -a : all topics specified under"  
+    echo "         -i : imu pose and raw data"
+    echo "         -t : data from all temperature sensors"
     echo "         -c : camera data; i.e. camera info, image rectified and color image from up and down cameras"
     echo "         -d : depth sensor data"
+    echo "         -v : voltages from both batteries"
+    echo "         -e : motor message, usb, solenoid, and pressure inside main pressure vessel" 
     echo "         -h : prints this menu"
+    exit
 }
 
 # Starts ROSBag with the given parameters.
@@ -22,7 +25,31 @@ startRosBagWithParameters() {
 	rosbag record $command_arguments -O ~/bag-$(date +%s)
 }
 
-# Handls the input of the scripts.
+addImuTopics() {
+	command_arguments="${command_arguments} state_estimation/pose state_estimation/raw"
+}
+
+addTemperatureTopics() {
+	command_arguments="${command_arguments} electrical_interface/batteryVoltage1 electrical_interface/batteryVoltage2"
+}
+
+addCameraTopics() {
+	command_arguments="${command_arguments} camera_down/camera_out/camera_info camera_down/image_rect camera_front_left/camera_out/camera_info camera_front_left/image_rect"
+}
+
+addDepthTopic() {
+	command_arguments="${command_arguments} electrical_interface/depth"
+}
+
+addVoltageTopics() {
+	echo ""
+}
+
+addElectricalInterfaceTopics() {
+	command_arguments="${command_arguments} status/usb electrical_interface/pressure electrical_interface/motor electrical_interface/solenoid"
+}
+
+# Handles the input of the scripts.
 handleInputParameters() {
 	for arg in "$@"; do
 		case ${arg} in
@@ -33,26 +60,21 @@ handleInputParameters() {
 				command_arguments="${command_arguments} status/temperature electrical_interface/temperature electrical_interface/temperature1 electrical_interface/temperature2 electrical_interface/temperature3 electrical_interface/temperature4 electrical_interface/temperature5"
 				command_arguments="${command_arguments} status/temperature electrical_interface/temperature"
 				command_arguments="${command_arguments} camera_down/camera_out/camera_info camera_down/image_rect camera_front_left/camera_out/camera_info camera_front_left/image_rect"
-				startRosBagWithParameters
 				;;
 			'-i' )
 				command_arguments="${command_arguments} state_estimation/pose"
-				startRosBagWithParameters
 				;;
 			'-t' )
 				command_arguments="${command_arguments} status/temperature electrical_interface/temperature electrical_interface/temperature1 electrical_interface/temperature2 electrical_interface/temperature3 electrical_interface/temperature4 electrical_interface/temperature5"
-				startRosBagWithParameters
 				;;
 			'-c' )
 				#down camera:
 				command_arguments="${command_arguments} camera_down/camera_out/camera_info camera_down/camera_out/image_raw"
 				#front camera
 				command_arguments="${command_arguments} camera_front_left/camera_out/camera_info camera_front_left/camera_out/image_raw"
-	            startRosBagWithParameters
 	            ;;
 			'-d' )
 				command_arguments="${command_arguments} electrical_interface/depth"
-				startRosBagWithParameters
 				;;
 			'-h' )
 				usage
@@ -62,6 +84,7 @@ handleInputParameters() {
 	            ;;
 		esac
 	done
+	startRosBagWithParameters
 }
 
 if [ "$#" -eq "0" ]
