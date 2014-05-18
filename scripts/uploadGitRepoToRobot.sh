f8b35cb6cd688a7ec4135f0e3d50edce6bf46f7e
Baycken <baycken@ubuntu.(none)>
2014-05-17 20:40:33 -0400

    upload bash change

--- scripts/uploadGitRepoToRobot.sh
+++ scripts/uploadGitRepoToRobot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sshpass -p 'elgordo21' scp -r catkin_ws/src robotics@10.0.0.1:~/McGill_RoboSub_2014/catkin_ws
-sshpass -p 'elgordo21' scp -r Arduino robotics@10.0.0.1:~/McGill_RoboSub_2014/
-sshpass -p 'elgordo21' scp -r dotfiles robotics@10.0.0.1:~/McGill_RoboSub_2014/
+sshpass -p 'elgordo21' scp -r ../catkin_ws/src robotics@10.0.0.1:~/McGill_RoboSub_2014/catkin_ws
+sshpass -p 'elgordo21' scp -r ../Arduino robotics@10.0.0.1:~/McGill_RoboSub_2014/
+sshpass -p 'elgordo21' scp -r ../setup robotics@10.0.0.1:~/McGill_RoboSub_2014/
