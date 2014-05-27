9ead06217a12b411693db63b42d2be5dc801960e
Anass Al-Wohoush <anass.alwohoush@outlook.com>
2014-05-26 20:02:09 -0400

    first draft of simulator

--- catkin_ws/src/hydrophones/scripts/param.py
+++ catkin_ws/src/hydrophones/scripts/param.py
@@ -8,9 +8,12 @@ BUFFERSIZE = 128            # SIZE OF FFT BUFFER
 NUMBER_OF_MICS = 4          # RECEIVERS CONNECTED
 SAMPLING_FREQUENCY = 192e3  # SAMPLING FREQUENCY OF SIGNAL  Hz
 TARGET_FREQUENCY = 1000     # FREQUENCY OF PINGER           Hz
+DEPTH_OF_PINGER = 4.2672    # DEPTH OF PINGER FROM SURFACE  m
 SPEED = 1500                # SPEED OF SOUND IN MEDIUM      m/s
 HEIGHT = 1.83               # HEIGHT OF RECEIVER ARRAY      m
 WIDTH = 0.91                # WIDTH OF RECEIVER ARRAY       m
+PINGER_X = 169.2
+PINGER_Y = 54.8
 
 
 def get_buffersize():
@@ -53,6 +56,14 @@ def get_target_frequency():
     return int(rospy.get_param('/hydrophones/target'))
 
 
+def get_depth_of_pinger():
+    """ Returns depth of the pinger from the surface in m """
+    while not rospy.has_param('/hydrophones/depth'):
+        pass
+
+    return rospy.get_param('/hydrophones/depth')
+
+
 def get_mic_positions():
     """ Returns (x,y) coordinates of every hydrophone """
     while not rospy.has_param('/hydrophones/pos/'):
@@ -67,13 +78,31 @@ def get_mic_positions():
     return pos
 
 
+def get_simulation_solution():
+    """ Returns (x,y) coordinates of the simulated pinger """
+    while not rospy.has_param('/hydrophones/simulation'):
+        pass
+
+    x = rospy.get_param('/hydrophones/simulation/x')
+    y = rospy.get_param('/hydrophones/simulation/y')
+
+    return (x,y)
+
+
+def set_simulation_parameters():
+    """ Creates and sets ROS simulation parameters """
+    rospy.set_param('/hydrophones/simulation/x',PINGER_X)
+    rospy.set_param('/hydrophones/simulation/y',PINGER_Y)
+
+
 def set_parameters():
-    """ Creates and sets ROS parameters """
+    """ Creates and sets default ROS parameters """
     rospy.set_param('/hydrophones/buffersize',BUFFERSIZE)
     rospy.set_param('/hydrophones/number_of_mics',NUMBER_OF_MICS)
     rospy.set_param('/hydrophones/speed',SPEED)
     rospy.set_param('/hydrophones/fs',SAMPLING_FREQUENCY)
     rospy.set_param('/hydrophones/target',TARGET_FREQUENCY)
+    rospy.set_param('/hydrophones/depth',DEPTH_OF_PINGER)
 
     rospy.set_param('/hydrophones/pos/0/x',0)
     rospy.set_param('/hydrophones/pos/0/y',0)
---
+++ catkin_ws/src/hydrophones/scripts/simulate_tdoa.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# IMPORTS
+import numpy as np
+import rospy
+import roslib
+from hydrophones.msg import *
+import time
+import param
+
+# PARAMETERS
+param.set_simulation_parameters()
+NUMBER_OF_MICS = param.get_number_of_mics()
+SPEED = param.get_speed()
+POS = param.get_mic_positions()
+DEPTH_OF_PINGER = param.get_depth_of_pinger()
+
+rospy.init_node('tdoa')
+tdoa_topic = rospy.Publisher('/hydrophones/time_difference',tdoa)
+rate = rospy.Rate(0.5)
+dt = tdoa()
+
+def compute_tdoa():
+    ''' Computes and publishes expected TDOA '''
+    (x,y) = param.get_simulation_solution()
+
+    times = []
+    for i in range(NUMBER_OF_MICS):
+        magnitude = np.sqrt((POS[i][0]-x)**2 + (POS[i][1]-y)**2)
+        times.append(magnitude/SPEED)
+
+    dt.tdoa_1 = times[1] - times[0]
+    dt.tdoa_2 = times[2] - times[0]
+    dt.tdoa_3 = times[3] - times[0]
+    tdoa_topic.publish(dt)
+
+if __name__ == '__main__':
+    try:
+        while not rospy.is_shutdown():
+            compute_tdoa()
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
