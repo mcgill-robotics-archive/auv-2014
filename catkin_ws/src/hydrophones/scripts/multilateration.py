96b3f002c661b9ece13ed65be03379fbc5e85929
Anass Al-Wohoush <anass.alwohoush@outlook.com>
2014-05-26 20:01:52 -0400

    working multilateration solver

--- catkin_ws/src/hydrophones/scripts/multilateration.py
+++ catkin_ws/src/hydrophones/scripts/multilateration.py
@@ -1,27 +1,61 @@
 #!/usr/bin/env python
 
-### IMPORTS
+# TODO: DISTINGUISH PRACTICE FROM COMPETITION
+#       
+
+# IMPORTS
+from ctypes import *
 import numpy as np
+import rospy
+import roslib
+from hydrophones.msg import *
+from geometry_msgs.msg import Point
+import param
 
-def solve(mics,speed):
+# PARAMETERS
+NUMBER_OF_MICS = param.get_number_of_mics()
+SPEED = param.get_speed()
+POS = param.get_mic_positions()
+DEPTH_OF_PINGER = param.get_depth_of_pinger()
+
+
+def solve(data):
     """ Solve by multilateration """
-    NUMBER_OF_MICS = len(mics)
+    # PARSE TDOA
+    dt = [0,data.tdoa_1,data.tdoa_2,data.tdoa_3]
 
+    # MULTILATERATE
     A = np.zeros(NUMBER_OF_MICS)
     B = np.zeros(NUMBER_OF_MICS)
     C = np.zeros(NUMBER_OF_MICS)
-
     for i in range(2,NUMBER_OF_MICS):
-        A[i] = (2*mics[i].pos.x) / (speed*mics[i].diff) - \
-               (2*mics[1].pos.x) / (speed*mics[1].diff)
+        A[i] = (2*POS[i][0]) / (SPEED*dt[i]) - \
+               (2*POS[1][0]) / (SPEED*dt[1])
 
-        B[i] = (2*mics[i].pos.y) / (speed*mics[i].diff) - \
-               (2*mics[1].pos.y) / (speed*mics[1].diff)
+        B[i] = (2*POS[i][1]) / (SPEED*dt[i]) - \
+               (2*POS[1][1]) / (SPEED*dt[1])
 
-        C[i] = speed*(mics[i].diff - mics[1].diff) - \
-               (mics[i].pos.x**2 + mics[i].pos.y**2) / (speed*mics[i].diff) + \
-               (mics[1].pos.x**2 + mics[1].pos.y**2) / (speed*mics[1].diff)
+        C[i] = SPEED*(dt[i] - dt[1]) - \
+               (POS[i][0]**2 + POS[i][1]**2) / (SPEED*dt[i]) + \
+               (POS[1][0]**2 + POS[1][1]**2) / (SPEED*dt[1])
 
+    # SOLVE BY QR
     (x,y) = -np.linalg.solve(np.transpose([A[2:],B[2:]]),C[2:])
 
-    return Point(x,y)
+    # PUBLISH
+    sol = Point()
+    sol.x = x
+    sol.y = y
+    sol.z = DEPTH_OF_PINGER
+    solver_topic.publish(sol)
+    
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('solver')
+        solver_topic = rospy.Publisher('/hydrophones/solution',Point)
+        rospy.Subscriber('/hydrophones/time_difference',tdoa,solve)
+        while not rospy.is_shutdown():
+            pass
+    except rospy.ROSInterruptException:
+        pass
