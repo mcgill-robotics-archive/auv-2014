fd0084273c7d443bafa5ee54d94ac0e7a7647066
Anass Al-Wohoush <anass.alwohoush@outlook.com>
2014-06-07 23:57:39 -0400

    wip alsa

---
+++ catkin_ws/src/hydrophones/scripts/alsa.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+# TODO: MAKE SURE LINE IN AND MIC WORK AS EXPECTED
+
+# IMPORTS
+import alsaaudio
+import numpy as np
+import rospy
+import roslib
+from hydrophones.msg import *
+import param
+
+#PARAMETERS
+BUFFERSIZE = param.get_buffersize()
+NUMBER_OF_MICS = param.get_number_of_mics()
+SAMPLING_FREQUENCY = param.get_sampling_frequency()
+PERIOD = 32
+
+# SET UP NODE AND TOPIC
+rospy.init_node('audio')
+audio_topic = rospy.Publisher('/hydrophones/audio', channels)
+signal = channels()
+
+
+def setup():
+    """ Sets up audio streams """
+    global inputs
+    mic = alsaaudio.PCM(type=alsaaudio.PCM_CAPTURE,card='default')
+    lin = alsaaudio.PCM(type=alsaaudio.PCM_CAPTURE,card='default')
+    inputs = {'mic':mic, 'lin':lin}
+
+    for card in inputs:
+        inputs[card].setchannels(2)
+        inputs[card].setrate(SAMPLING_FREQUENCY)
+        inputs[card].setperiodsize(PERIOD)
+        inputs[card].setformat(alsaaudio.PCM_FORMAT_FLOAT_LE)
+
+
+def read():
+    """ Reads and parses audio streams """
+    stream = []
+    for counter in range(BUFFERSIZE/PERIOD):
+        current_stream = []
+        for card in inputs:
+            data = np.fromstring(inputs[card].read()[1],dtype=np.float32)
+            current_stream.extend([data[::2],data[1::2]])
+        if counter != 0:
+            stream.append(current_stream)
+        else:
+            np.append(stream,current_stream,1)
+
+    signal.channel_0 = stream[0]
+    signal.channel_1 = stream[1]
+    signal.channel_2 = stream[2]
+    signal.channel_3 = stream[3]
+
+    audio_topic.publish(signal)
+
+
+def close():
+    """ Closes audio streams """
+    for card in inputs:
+        inputs[card].close()
+
+
+if __name__ == '__main__':
+    try:
+        setup()
+        while not rospy.is_shutdown():
+            read()
+    except rospy.ROSInterruptException:
+        pass
+
+    close()
--- catkin_ws/src/hydrophones/scripts/audio.py
+++ catkin_ws/src/hydrophones/scripts/audio.py
@@ -32,27 +32,32 @@ c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
 asound = cdll.LoadLibrary('libasound.so')
 asound.snd_lib_error_set_handler(c_error_handler)
 
-### SET UP SOUND INPUT
-audio = pyaudio.PyAudio()
-mic = audio.open(format=pyaudio.paFloat32, channels=2,
-                 rate=SAMPLING_FREQUENCY, input=True,
-                 frames_per_buffer=BUFFERSIZE)
-lin = audio.open(format=pyaudio.paFloat32, channels=2,
-                 rate=SAMPLING_FREQUENCY, input=True,
-                 frames_per_buffer=BUFFERSIZE)
+
+def setup():
+    """ Sets up audio streams """
+    global auudio, inputs
+    audio = pyaudio.PyAudio()
+    mic = audio.open(format=pyaudio.paFloat32, channels=2,
+                     rate=SAMPLING_FREQUENCY, input=True,
+                     frames_per_buffer=BUFFERSIZE)
+    lin = audio.open(format=pyaudio.paFloat32, channels=2,
+                     rate=SAMPLING_FREQUENCY, input=True,
+                     frames_per_buffer=BUFFERSIZE)
+    inputs = {'mic':mic, 'lin':lin}
 
 
 def read():
     ''' Reads signal from microphones '''
-    data_mic = np.fromstring(mic.read(BUFFERSIZE),
+    stream = []
+    for card in inputs:
+        data = np.fromstring(inputs[card].read(BUFFERSIZE),
                              dtype=np.float32)
-    data_lin = np.fromstring(lin.read(BUFFERSIZE),
-                             dtype=np.float32)
+        stream.extend([data[::2],data[1::2]])
 
-    signal.channel_0 = data_mic[::2]
-    signal.channel_1 = data_mic[1::2]
-    signal.channel_2 = data_lin[::2]
-    signal.channel_3 = data_lin[1::2]
+    signal.channel_0 = stream[0]
+    signal.channel_1 = stream[1]
+    signal.channel_2 = stream[2]
+    signal.channel_3 = stream[3]
 
     audio_topic.publish(signal)
 
@@ -61,10 +66,12 @@ def close():
     ''' Closes audio streams '''
     audio.close(mic)
     audio.close(lin)
+    audio.terminate()
 
 
 if __name__ == '__main__':
     try:
+        setup()
         while not rospy.is_shutdown():
             read()
     except rospy.ROSInterruptException:
