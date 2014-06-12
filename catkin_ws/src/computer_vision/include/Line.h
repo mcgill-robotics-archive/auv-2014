edef945520d41c7f4cc3821dc50c79437ce16528
Thuy-Anh <thuy-anh@thuyanh-S400CA.(none)>
2014-06-11 22:52:08 -0400

    Updated the Line filter so that there is different filters for differents videos and added a zDistance in the down_cv/data output

--- catkin_ws/src/computer_vision/include/Line.h
+++ catkin_ws/src/computer_vision/include/Line.h
@@ -14,22 +14,31 @@
 using namespace cv;
 using namespace std;
 
+double camera_focal_length;
+
+double camera_sensor_height;
+
+
+
+
 class Line : public VisibleObject {
 
 	double xDistance;
 	double yDistance;
+	double zDistance;
 	double yaw;
+
 	public:
 	Line(const CVNode& _parent);
 	std::vector<computer_vision::VisibleObjectData*> retrieveObjectData(cv::Mat& currentFrame);
 
 	double convertFromPixelsToMetres(int distance, double longSize);
 	void applyFilter(cv::Mat& image);
-	void thresh_callback(int, void* );
+	// Line::thresh_callback(int, void* );
 	double relativeYaw(cv::RotatedRect line);
 	bool isVisible(cv::RotatedRect line);
 	int add(int x, int y);
-
+	double calculateApproxDistanceToLine (double yaw, double sizeLong, double sizeWide, cv::Mat& currentFrame);
 };
 
 #endif /* LINE_H */
