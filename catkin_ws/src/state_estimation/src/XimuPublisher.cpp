053c17f5964fa415cdaabd44d3a4b22e5a169341
Anass Al-Wohoush <anass.alwohoush@outlook.com>
2014-06-15 23:11:15 -0400

    formatted correctly for bearable reading

--- catkin_ws/src/state_estimation/src/XimuPublisher.cpp
+++ catkin_ws/src/state_estimation/src/XimuPublisher.cpp
@@ -1,4 +1,4 @@
-#include <boost/asio.hpp> 
+#include <boost/asio.hpp>
 #include <boost/asio/serial_port.hpp>
 
 #include "XimuReceiver.h"
@@ -17,7 +17,7 @@ static unsigned int sequence = 0;
 ros::Publisher pub, pub2, rawPub;
 geometry_msgs::Pose pos;
 geometry_msgs::PoseStamped posStamped;
-double prerotation[] = {1.0/sqrt(2.0), 1.0/sqrt(2.0),0.0,0.0};
+double prerotation[] = {1.0/sqrt(2.0), 1.0/sqrt(2.0), 0.0, 0.0};
 double* q = new double[4];
 asio::io_service serial_io;
 asio::serial_port port(serial_io);
@@ -36,7 +36,6 @@ void multiplyQuaternions(double q[],double p[])
     q[3] = temp[3];
 }
 
-
 void spin() {
 	port.open("/dev/ttyUSB0");
 	port.set_option(asio::serial_port_base::baud_rate(115200));
@@ -49,38 +48,38 @@ void spin() {
 		asio::read(port, asio::buffer(&c,1));
 		receiver.processNewChar(c);
 
-if (receiver.isInertialAndMagGetReady()) {
-                        InertialAndMagStruct ims = receiver.getInertialAndMag();
-                        geometry_msgs::Vector3 acc = geometry_msgs::Vector3();
-                        acc.x = ims.accX;
-                        acc.y = ims.accY;
-                        acc.z = ims.accZ;
-                        pub2.publish(acc);
+        if (receiver.isInertialAndMagGetReady()) {
+            InertialAndMagStruct ims = receiver.getInertialAndMag();
+            geometry_msgs::Vector3 acc = geometry_msgs::Vector3();
+            acc.x = ims.accX;
+            acc.y = ims.accY;
+            acc.z = ims.accZ;
+            pub2.publish(acc);
 
-                        // IMU raw data
-                        sensor_msgs::Imu imu;
-                        imu.angular_velocity.x = ims.gyrX;
-                        imu.angular_velocity.y = ims.gyrY;
-                        imu.angular_velocity.z = ims.gyrZ;
-                        imu.linear_acceleration.x = ims.accX;
-                        imu.linear_acceleration.y = ims.accY;
-                        imu.linear_acceleration.z = ims.accZ;
-                        rawPub.publish(imu);
-                }		
+            // IMU raw data
+            sensor_msgs::Imu imu;
+            imu.angular_velocity.x = ims.gyrX;
+            imu.angular_velocity.y = ims.gyrY;
+            imu.angular_velocity.z = ims.gyrZ;
+            imu.linear_acceleration.x = ims.accX;
+            imu.linear_acceleration.y = ims.accY;
+            imu.linear_acceleration.z = ims.accZ;
+            rawPub.publish(imu);
+        }
 
-if (receiver.isQuaternionGetReady()) {
+        if (receiver.isQuaternionGetReady()) {
 			QuaternionStruct quaternionStruct = receiver.getQuaternion();
-			
+
  			q[0] = quaternionStruct.w;
 			q[1] = quaternionStruct.x;
 			q[2] = quaternionStruct.y;
 			q[3] = quaternionStruct.z;
-    	    		multiplyQuaternions(q, prerotation);
+            multiplyQuaternions(q, prerotation);
 			pos.orientation.w = q[0];
 			pos.orientation.x = q[1];
 			pos.orientation.y = q[2];
 			pos.orientation.z = q[3];
-			
+
 			posStamped = geometry_msgs::PoseStamped();
 			posStamped.pose = pos;
 			posStamped.header.seq = sequence++;
@@ -89,9 +88,6 @@ if (receiver.isQuaternionGetReady()) {
 
 			pub.publish(posStamped);
 		}
-
-		
-		        
 	}
 }
 
@@ -104,8 +100,8 @@ int main(int argc, char** argv) {
     pub2 = node.advertise<geometry_msgs::Vector3>("state_estimation/acc", 100);
     // Publish raw data
     rawPub = node.advertise<sensor_msgs::Imu>("state_estimation/raw", 100);
-	
+
 	spin();
-	
+
 	return 0;
 }
